c002ae39e299a2fe447622ad4d6859000a1661ce
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-04-04 04:14:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,8 +10,9 @@ end
 
 def countdown_with_sleep(time)
   while number > 0 
-    puts "#{number} SECOND(S)!".sleep(1)
+    puts "#{number} SECOND(S)!"
     number -= 1
+    sleep(1)
   end 
   "HAPPY NEW YEAR!"
 end
